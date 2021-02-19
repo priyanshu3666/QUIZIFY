d2b90982cd13554a6737bf772193d09cae1651b1
Priyanshu Shukla <priyanshu.shukla_cs18@gla.ac.in>
2021-02-19 21:58:37 +0530

    project completed

--- Quizify/app/src/main/java/com/example/quizify/StartActivity.java
+++ Quizify/app/src/main/java/com/example/quizify/StartActivity.java
@@ -41,7 +41,7 @@ public class StartActivity extends AppCompatActivity {
             @Override
             public void run() {
                 try {
-                    Thread.sleep(3000);
+                    Thread.sleep(2000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
